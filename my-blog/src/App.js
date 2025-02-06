d2bd6a0e7ecdb5dec7acc91458ddeeeb5cc5357f
KOTAPHANITEJA <phanitejakota@gmail.com>
2025-02-06 10:47:00 +0530

    Initial Commit

---
+++ my-blog/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar.js'; 
+import Home from './home.js'; 
+function App() { 
+  return ( 
+    <div className="App"> 
+      <Navbar/> 
+      <div className="content">Hello World</div> 
+      <Home /> 
+    </div> 
+  ); 
+} 
+export default App;
