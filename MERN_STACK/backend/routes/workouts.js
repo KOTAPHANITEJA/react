d3e31ae8410965700fe45c24bb5b77d00503a93c
KOTAPHANITEJA <phanitejakota@gmail.com>
2025-02-08 10:26:16 +0530

    Second Commit

---
+++ MERN_STACK/backend/routes/workouts.js
@@ -0,0 +1,11 @@
+const express=require('express')
+
+const router = express.Router()
+
+
+// GET all workouts
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST request output'})
+  })
+
+module.exports=router
--- MERN_STACK/backend/server.js
+++ MERN_STACK/backend/server.js
@@ -1,11 +1,23 @@
 require('dotenv').config()
 
 const express = require('express')
+
 const app=express()
 
-app.get('/',(req,res) => {
-    res.json({mssg: "Welcome to the app."})
-})
+const workoutRoutes=require('./routes/workouts')
+
+app.use(express.json())
+
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+  })
+  
+//   // routes
+app.use('/api/workouts', workoutRoutes)
+
+
 
 app.listen(process.env.PORT, () => {
     console.log('Listening at port 4000!!!!',process.env.PORT)
